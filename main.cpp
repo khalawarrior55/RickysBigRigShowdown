73d73d1fa3fb6b7429ea8f9d1992c4b0916ecbbb
khalawarrior55 <ryanyoung1@hotmail.com>
2016-10-11 14:25:02 -0400

    Added fps Counter

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
+#include <string>
 #include "Entity.h"
 #include "Player.h"
 
@@ -10,23 +11,41 @@ int main(int argc, char ** argv)
 {
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    //window.setFramerateLimit(60);
+
+    // Create a clock for the framerate
+    sf::Clock clock;
+
     // Load a sprite to display
     sf::Texture texture;
     if (!texture.loadFromFile("shit.png"))
         return EXIT_FAILURE;
+
+    // Load the arial Font
+    sf::Font arial;
+    if (!arial.loadFromFile("arial.ttf"))
+        return EXIT_FAILURE;
+
+    sf::Text fps;
+    fps.setFont(arial);
+    fps.setColor(sf::Color::White);
+
+
     // Create a graphical text to display
     // Load a music to play
     // sf::Music music;
     // if (!music.openFromFile("nice_music.ogg"))
     //     return EXIT_FAILURE;
-    // Play the music
-    // music.play();
-    // Start the game loop
+    // Creates the pizza texture
     Player pizza(texture);
     pizza.setWindow(window);
+
+    // Creates a floor on screen
     sf::RectangleShape floor(sf::Vector2f(800, 10));
     floor.setFillColor(sf::Color::Blue);
     floor.setPosition(sf::Vector2f(0, 450));
+
+    // Begin Game loop
     while (window.isOpen())
     {
         // Process events
@@ -44,6 +63,17 @@ int main(int argc, char ** argv)
         window.draw(floor);
         pizza.update();
         pizza.draw();
+
+        // Calculate Framerate
+        sf::Time frTime = clock.getElapsedTime();
+        std::cout << 1.0f / frTime.asSeconds() << std::endl;        
+        int fpsFloat = 1.0f / frTime.asSeconds();
+        string fpsString = "FPS: " + std::to_string(fpsFloat);
+        fps.setString(fpsString);
+
+        window.draw(fps);
+        
+        clock.restart().asSeconds();
 
         // Update the window
         window.display();
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 test: main.o Entity.o Player.o
 	g++ main.o Entity.o Player.o -o test.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-system -lsfml-audio
 main.o: main.cpp
-	g++ -c main.cpp
+	g++ -std=c++11 -c main.cpp
 Entity.o: Entity.cpp
 	g++ -c Entity.cpp
 Player.o: Player.cpp
